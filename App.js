59d1d7579efd8812367e3d2cf311e9473aecdbe2
Android Studio <devandroid@ort>
2025-05-12 09:02:30 -0300

    Worked on Card.js

--- App.js
+++ App.js
@@ -1,27 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Card from './components/card';
 
 const imageBackground = require('./assets/imageBackground.png');
+const profile = require('./assets/profile.png');
+const username = 'Sati';
+const title = 'Grguera';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView>
       <StatusBar style="auto" />
-      <ImageBackground source={imageBackground} resizeMode='stretch' style={styles.image}>
-
+      <ImageBackground source={imageBackground} resizeMode='cover' style={styles.image}>
+        <View style={styles.card}>
+          <Card profile={profile} username={username} title={title}/>
+        </View>
       </ImageBackground>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
   image: {
     
   }
